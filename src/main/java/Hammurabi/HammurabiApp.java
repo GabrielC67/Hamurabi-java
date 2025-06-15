c50bb510662509f4825bb3c03a924fe7293769f7
GabrielC67 <gabecruzpro@gmail.com>
2025-06-15 17:55:29 -0400

    Checkpoint

--- src/main/java/Hammurabi/HammurabiApp.java
+++ src/main/java/Hammurabi/HammurabiApp.java
@@ -87,44 +87,43 @@ public class HammurabiApp {
             return feedPeople;
         }
 
-    int askHowManyAcresToPlant(int acresOwned, int population, int bushels){
-        //User Input
-        int acresToPlant = getNumber("O great Hammurabi, how many acres of land will you plant?");
+    int askHowManyAcresToPlant(int acresOwned, int population, int bushels) {
+        //This loop is to allow the user to re-input another number should there be not enough bushels.
+        while (true) {
+            try {
+                int acresToPlant = getNumber("O great Hammurabi, how many acres of land will you plant?");
+                //I think I need to make a Map of people that have a range of 0-10 to be able to plant acres based on what user can input.
+                Map<Integer, Integer> individualPersonList = new HashMap<>();
 
-        //I think I need to make a Map of people that have a range of 0-10 to be able to plant acres based on what user can input.
-        Map<Integer, Integer> individualPersonList = new HashMap<>();
+                //This will Iterate through each person and finds out how many acres each person in the population is able to plant.
+                int personID = 1;
+                while (personID <= population) {
+                    int acresThatPersonPlants = rand.nextInt(11);
+                    individualPersonList.put(personID, acresThatPersonPlants);
+                    //After one person is added to the list
+                    personID++;
+                }
 
+                //This sums up the total acres produced from each person.
+                int sumOfAcresProducedFromEachPerson = 0;
+                for (int numberOfAcresProduced : individualPersonList.values()) {
+                    sumOfAcresProducedFromEachPerson += numberOfAcresProduced;
+                }
 
-        //This will Iterate through each person and finds out how many acres each person in the population is able to plant.
-        int personID = 1;
-        while(personID <= population){
-            int acresThatPersonPlants = rand.nextInt(11);
-            individualPersonList.put(personID, acresThatPersonPlants);
-            //After one person is added to the list
-            personID++;
-        }
-
-        //This sums up the total acres produced from each person.
-        int sumOfAcresProducedFromEachPerson = 0;
-        for(int numberOfAcresProduced : individualPersonList.values()) {
-            sumOfAcresProducedFromEachPerson += numberOfAcresProduced;
-        }
-
-        /*If the sum of acres produced surpasses or is equal to what user inputs, AND there's the bushels to do the work
-        then we can return what the user inputs without errors. Both conditions must be true*/
-        if(acresToPlant <= sumOfAcresProducedFromEachPerson && acresToPlant <= acresOwned) {
-            if (bushels >= (acresToPlant * 2)) {
-                //This would mean I need to subract what is returned * 2 for the bushels AND, I need to add to acres.
-                return acresToPlant;
-            } else {
-                //If there's not enough bushels for the job, then the job is not doable at all.
-                return 0;
+                /*If the sum of acres produced surpasses or is equal to what user inputs, AND there's the bushels to do the work
+                then we can return what the user inputs without errors. Both conditions must be true*/
+                if (acresToPlant <= sumOfAcresProducedFromEachPerson && acresToPlant <= acresOwned) {
+                    if (bushels >= (sumOfAcresProducedFromEachPerson * 2)) {
+                        //This would mean I need to subtract what is returned * 2 for the bushels AND, I need to add to acres.
+                        return sumOfAcresProducedFromEachPerson;
+                    }
+                }
+            } catch (ArithmeticException e) {
+                System.out.println("There's not enough bushels to do this job! Please enter another number.");
             }
-        } else {
-            //If the people cannot produce what Hammurabi demands, then return what the population could produce.
-            return sumOfAcresProducedFromEachPerson;
         }
     }
+
 
 //    int plagueDeaths(int population)
 //
